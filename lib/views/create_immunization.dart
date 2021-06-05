ed446e204c468e54bc02310d896bbbaf0f2f1649
Dan Heslinga <drdan@heslingamd.com>
2021-06-05 11:48:58 -1000

    comment out create_immunization

--- lib/views/create_immunization.dart
+++ lib/views/create_immunization.dart
@@ -12,18 +12,18 @@
 /// [doseQuantity]: The quantity of vaccine product that was administered.
 /// [performer]: Indicates who performed the immunization event.
 
-import 'package:fhir/r4.dart';
+// import 'package:fhir/r4.dart';
 
-CodeableConcept? vaccineCode;
-Reference? patient;
-FhirDateTime? occurrenceDateTime;
-Reference? manufacturer;
-String? lotNumber;
-Date? expirationDate;
-CodeableConcept? site;
-CodeableConcept? route;
-Quantity? doseQuantity;
-List<ImmunizationPerformer>? performer;
+// CodeableConcept? vaccineCode;
+// Reference? patient;
+// FhirDateTime? occurrenceDateTime;
+// Reference? manufacturer;
+// String? lotNumber;
+// Date? expirationDate;
+// CodeableConcept? site;
+// CodeableConcept? route;
+// Quantity? doseQuantity;
+// List<ImmunizationPerformer>? performer;
 
 // import 'package:fhir/r4.dart';
 // import 'package:fhir_at_rest/r4.dart';
@@ -75,88 +75,88 @@ List<ImmunizationPerformer>? performer;
 //     return Scaffold(
 //       body: Column(
 //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-import 'package:fhir/r4.dart';
-import 'package:fhir_at_rest/r4.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
-import '../controllers/patient_gender_controller.dart';
-import 'patient_gender_view.dart';
-import 'date_picker.dart';
-import 'small_action_button.dart';
-import 'name_container.dart';
+// import 'package:fhir/r4.dart';
+// import 'package:fhir_at_rest/r4.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:url_launcher/url_launcher.dart';
+// import '../controllers/patient_gender_controller.dart';
+// import 'patient_gender_view.dart';
+// import 'date_picker.dart';
+// import 'small_action_button.dart';
+// import 'name_container.dart';
 // import 'create_patient.dart';
 
-class CreateImmunization extends StatelessWidget {
-  //final PatientGenderController controller = Get.put(PatientGenderController());
+// class CreateImmunization extends StatelessWidget {
+//   //final PatientGenderController controller = Get.put(PatientGenderController());
 
-  @override
-  Widget build(BuildContext context) {/*
-    final _lastName = TextEditingController();
-    final _firstName = TextEditingController();
-    final _birthDateController = TextEditingController();
-    final _idController = TextEditingController();*/
-    CodeableConcept? vaccineCode;
-Reference? patient;
-FhirDateTime? occurrenceDateTime;
-Reference? manufacturer;
-String? lotNumber; // yeah, string
-Date? expirationDate;  // Use DatePicker
-CodeableConcept? site; // dropdown list
-CodeableConcept? route; // dropdown list
-Quantity? doseQuantity; // text field
-List<ImmunizationPerformer>? performer; // ?????
+//   @override
+//   Widget build(BuildContext context) {/*
+//     final _lastName = TextEditingController();
+//     final _firstName = TextEditingController();
+//     final _birthDateController = TextEditingController();
+//     final _idController = TextEditingController();*/
+//     CodeableConcept? vaccineCode;
+// Reference? patient;
+// FhirDateTime? occurrenceDateTime;
+// Reference? manufacturer;
+// String? lotNumber; // yeah, string
+// Date? expirationDate;  // Use DatePicker
+// CodeableConcept? site; // dropdown list
+// CodeableConcept? route; // dropdown list
+// Quantity? doseQuantity; // text field
+// List<ImmunizationPerformer>? performer; // ?????
 
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          //* Hapi FHIR calls
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              nameContainer(_idController, 'id'),
-              nameContainer(_lastName, 'Last name'),
-              nameContainer(_firstName, 'First name'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).copyWith().size.height / 3,
-                width: MediaQuery.of(context).copyWith().size.width / 3,
-                child: DatePicker(birthDateController: _birthDateController),
-              ),
-              GenderPicker(),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              SmallActionButton(
-                  title: 'Hapi: Create',
-                  onPressed: () => _hapiCreate(
-                        lastName: _lastName.text,
-                        firstName: _firstName.text,
-                        birthDate: _birthDateController.text,
-                        gender: controller.patientGender.value,
-                      )),
-              SmallActionButton(
-                title: 'Hapi: Search',
-                onPressed: () => _hapiSearch(
-                  lastName: _lastName.text,
-                  firstName: _firstName.text,
-                  birthDate: _birthDateController.text,
-                  id: _idController.text,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           //* Hapi FHIR calls
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: <Widget>[
+//               nameContainer(_idController, 'id'),
+//               nameContainer(_lastName, 'Last name'),
+//               nameContainer(_firstName, 'First name'),
+//             ],
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: <Widget>[
+//               Container(
+//                 height: MediaQuery.of(context).copyWith().size.height / 3,
+//                 width: MediaQuery.of(context).copyWith().size.width / 3,
+//                 child: DatePicker(birthDateController: _birthDateController),
+//               ),
+//               GenderPicker(),
+//             ],
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: <Widget>[
+//               SmallActionButton(
+//                   title: 'Hapi: Create',
+//                   onPressed: () => _hapiCreate(
+//                         lastName: _lastName.text,
+//                         firstName: _firstName.text,
+//                         birthDate: _birthDateController.text,
+//                         gender: controller.patientGender.value,
+//                       )),
+//               SmallActionButton(
+//                 title: 'Hapi: Search',
+//                 onPressed: () => _hapiSearch(
+//                   lastName: _lastName.text,
+//                   firstName: _firstName.text,
+//                   birthDate: _birthDateController.text,
+//                   id: _idController.text,
+//                 ),
+//               ),
+//             ],
+//           )
+//         ],
+//       ),
+//     );
+//   }
 
 //   Future _hapiCreate(
 //       {String lastName = '',
