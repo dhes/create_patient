b34b068fd87a1cb68e172942c1c818c8cd9cf844
Dan Heslinga <drdan@heslingamd.com>
2021-06-04 18:51:05 -1000

    how many of these vaccination fields should i start with?

--- lib/views/create_immunization.dart
+++ lib/views/create_immunization.dart
@@ -50,63 +50,113 @@ List<ImmunizationPerformer>? performer;
 //     return Scaffold(
 //       body: Column(
 //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           //* Hapi FHIR calls
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: <Widget>[
-//               nameContainer(_idController, 'id'),
-//               nameContainer(_lastName, 'Last name'),
-//               nameContainer(_firstName, 'First name'),
-//             ],
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: <Widget>[
-//               Container(
-//                 height: MediaQuery.of(context).copyWith().size.height / 3,
-//                 width: MediaQuery.of(context).copyWith().size.width / 3,
-//                 child: DatePicker(birthDateController: _birthDateController),
-//               ),
-//               GenderPicker(),
-//             ],
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: <Widget>[
-//               SmallActionButton(
-//                   title: 'Hapi: Create',
-//                   onPressed: () => _hapiCreate(
-//                         lastName: _lastName.text,
-//                         firstName: _firstName.text,
-//                         birthDate: _birthDateController.text,
-//                         gender: controller.patientGender.value,
-//                       )),
-//               SmallActionButton(
-//                 title: 'Hapi: Search',
-//                 onPressed: () => _hapiSearch(
-//                   lastName: _lastName.text,
-//                   firstName: _firstName.text,
-//                   birthDate: _birthDateController.text,
-//                   id: _idController.text,
-//                 ),
-//               ),
-//             ],
-//           )
-//         ],
-//       ),
-//     );
-//   }
+// import 'package:fhir/r4.dart';
+// import 'package:fhir_at_rest/r4.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:url_launcher/url_launcher.dart';
+// import '../controllers/patient_gender_controller.dart';
+// import 'patient_gender_view.dart';
+// import 'date_picker.dart';
+// import 'small_action_button.dart';
+// import 'name_container.dart';
+// // import 'create_patient.dart';
 
-//   // Container _nameContainer(TextEditingController name, String text) =>
-//   //     Container(
-//   //       width: Get.width / 4,
-//   //       margin: EdgeInsets.symmetric(horizontal: 8),
-//   //       child: TextField(
-//   //         controller: name,
-//   //         decoration: InputDecoration(hintText: text),
-//   //       ),
-//   //     );
+// class CreatePatient extends StatelessWidget {
+//   final PatientGenderController controller = Get.put(PatientGenderController());
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final _lastName = TextEditingController();
+//     final _firstName = TextEditingController();
+//     final _birthDateController = TextEditingController();
+//     final _idController = TextEditingController();
+
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+import 'package:fhir/r4.dart';
+import 'package:fhir_at_rest/r4.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../controllers/patient_gender_controller.dart';
+import 'patient_gender_view.dart';
+import 'date_picker.dart';
+import 'small_action_button.dart';
+import 'name_container.dart';
+// import 'create_patient.dart';
+
+class CreateImmunization extends StatelessWidget {
+  //final PatientGenderController controller = Get.put(PatientGenderController());
+
+  @override
+  Widget build(BuildContext context) {/*
+    final _lastName = TextEditingController();
+    final _firstName = TextEditingController();
+    final _birthDateController = TextEditingController();
+    final _idController = TextEditingController();*/
+    CodeableConcept? vaccineCode;
+Reference? patient;
+FhirDateTime? occurrenceDateTime;
+Reference? manufacturer;
+String? lotNumber; // yeah, string
+Date? expirationDate;  // Use DatePicker
+CodeableConcept? site; // dropdown list
+CodeableConcept? route; // dropdown list
+Quantity? doseQuantity; // text field
+List<ImmunizationPerformer>? performer; // ?????
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          //* Hapi FHIR calls
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              nameContainer(_idController, 'id'),
+              nameContainer(_lastName, 'Last name'),
+              nameContainer(_firstName, 'First name'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).copyWith().size.height / 3,
+                width: MediaQuery.of(context).copyWith().size.width / 3,
+                child: DatePicker(birthDateController: _birthDateController),
+              ),
+              GenderPicker(),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              SmallActionButton(
+                  title: 'Hapi: Create',
+                  onPressed: () => _hapiCreate(
+                        lastName: _lastName.text,
+                        firstName: _firstName.text,
+                        birthDate: _birthDateController.text,
+                        gender: controller.patientGender.value,
+                      )),
+              SmallActionButton(
+                title: 'Hapi: Search',
+                onPressed: () => _hapiSearch(
+                  lastName: _lastName.text,
+                  firstName: _firstName.text,
+                  birthDate: _birthDateController.text,
+                  id: _idController.text,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 
 //   Future _hapiCreate(
 //       {String lastName = '',
