127c26dd9e022f8321c45545c32ec64226674c17
Dan Heslinga <drdan@heslingamd.com>
2021-06-14 16:38:12 -1000

    patient summary at top; deals with missing birthday; deals with no patient found

--- lib/screens/display_patient.dart
+++ lib/screens/display_patient.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-//import 'dart:html';
 import 'package:fhir/r4.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
@@ -8,8 +7,12 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../views/small_action_button.dart';
+import 'package:intl/intl.dart';
+import 'package:age_calculator/age_calculator.dart';
 
-Future<Patient> fetchPatient({String? lastName, String? firstName}) async {
+enum Gender { F, M, O, U }
+
+Future<Patient?> fetchPatient({String? lastName, String? firstName}) async {
   final response = await http.get(
     Uri.parse('http://hapi.fhir.org/baseR4/Patient?'
         'family=$lastName&'
@@ -20,19 +23,21 @@ Future<Patient> fetchPatient({String? lastName, String? firstName}) async {
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    // Map<String, dynamic> map = jsonDecode(response.body);
-    // print(map['resourceType']);
-    // print('Hello');
-    // print(jsonDecode(response.body)['resourceType']);
-    // print(Bundle.fromJson(jsonDecode(response.body)));
-    // print('Hello');
-    // print(Bundle.fromJson(jsonDecode(response.body)).entry?[0]);
-    // print('Hello Patient');
-    // print(Patient.fromJson(jsonDecode(response.body)['entry'][0]['resource'])
-    //     .name![0]
-    //     .toString());
-//    return Patient.fromJson(jsonDecode(response.body));
-    return Patient.fromJson(jsonDecode(response.body)['entry'][0]['resource']);
+    // Are there any patients in the bundle?
+    if (jsonDecode(response.body)['total'] != 0) {
+      // If yes pass the first patient in the list to the widget
+      return Patient.fromJson(
+          jsonDecode(response.body)['entry'][0]['resource']);
+    } else {
+      Get.rawSnackbar(
+          title: 'Oops!',
+          message: 'I can\'t find any patients with that name. ');
+      await new Future.delayed(const Duration(seconds: 4));
+      Get.toNamed('/');
+    }
+    /*else {
+      throw Exception('No patients found');
+    }*/
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
@@ -48,7 +53,7 @@ class DisplayPatient extends StatefulWidget {
 }
 
 class _DisplayPatient extends State<DisplayPatient> {
-  late Future<Patient> futurePatient;
+  late Future<Patient?> futurePatient;
 
   @override
   void initState() {
@@ -63,69 +68,122 @@ class _DisplayPatient extends State<DisplayPatient> {
   Widget build(BuildContext context) {
     var nameController = TextEditingController();
     var genderController = TextEditingController();
+    //DateTime? birthday;
+    //DateTime? birthday;
+    int? age;
     return MaterialApp(
-      title: 'Patient Information',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
+        //title: 'Patient Information',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home:
+            /*Scaffold(
         appBar: AppBar(
           title: Text('Patient Information'),
         ),
-        body: FutureBuilder<Patient>(
+        body: */
+            FutureBuilder<Patient?>(
           future: futurePatient,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
+              print('birthday: ' + snapshot.data!.birthDate.toString());
+              if (snapshot.data!.birthDate != null) {
+                var birthday = (DateTime.parse(
+                    snapshot.data!.birthDate.toString() + ' 00:00'));
+                int age = AgeCalculator.age(birthday).years;
+                String dob = DateFormat.yMd()
+                    .format(DateTime.parse(
+                        snapshot.data!.birthDate.toString() + ' 00:00'))
+                    .replaceAll('/', '-');
+                genderController.text = age.toString() +
+                    'yo ' +
+                    _pickGender(snapshot.data!.gender) +
+                    ' ∙ DOB: ' +
+                    dob;
+              } else {
+                genderController.text = 'age? gender: ' +
+                    _pickGender(snapshot.data!.gender) +
+                    ' birthday ?';
+              }
               nameController.text =
                   snapshot.data!.name!.first.given!.first.toString() +
                       ' ' +
                       snapshot.data!.name!.first.family.toString();
-              genderController.text =
-                  describeEnum(snapshot.data!.gender.toString());
-              return ListView(children: <Widget>[
-                Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(children: <Widget>[
-                      TextField(
+              // }
+              return Material(
+                child: ListView(children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(children: <Widget>[
+                        TextField(
+                          textAlign: TextAlign.center,
                           controller: nameController,
-                          decoration: InputDecoration(
-                            labelText: 'Name',
-                            icon: Icon(Icons.perm_identity),
-                          )),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            flex: 4,
-                            child: TextField(
-                                controller: genderController,
-                                decoration: InputDecoration(
-                                  labelText: 'Gender',
-                                  icon: Icon(Icons.female),
-                                )),
+                          readOnly: true,
+                          decoration: null,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
                           ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SmallActionButton(
-                              title: 'Done',
-                              onPressed: () {
-                                Get.toNamed("/");
-                              }),
-                        ],
-                      )
-                    ]))
-              ]);
+                          /*decoration: InputDecoration(
+                              labelText: 'Name',
+                              icon: Icon(Icons.perm_identity),
+                            )*/
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                              flex: 4,
+                              child: TextField(
+                                controller: genderController,
+                                decoration: null,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.black.withOpacity(0.5),
+                                    fontSize: 14.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SmallActionButton(
+                                title: 'Done',
+                                onPressed: () {
+                                  Get.toNamed("/");
+                                }),
+                          ],
+                        )
+                      ]))
+                ]),
+              );
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             }
             // By default, show a loading spinner.
             return Center(child: CircularProgressIndicator());
           },
-        ),
+        ));
+    /*,
       ),
-    );
+    );*/
+  }
+}
+
+String _pickGender(PatientGender? longGender) {
+  switch (longGender) {
+    case PatientGender.female:
+      return describeEnum(Gender.F.toString());
+    //break;
+    case PatientGender.male:
+      return describeEnum(Gender.M.toString());
+    //break;
+    case PatientGender.other:
+      return describeEnum(Gender.O.toString());
+    //break;
+    case PatientGender.unknown:
+      return describeEnum(Gender.U.toString());
+    default:
+      return describeEnum(Gender.U.toString());
   }
 }
