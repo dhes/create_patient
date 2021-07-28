225f6e2d9f0a287f5f08d5df9165c27430fca265
Dan Heslinga <drdan@heslingamd.com>
2021-07-27 19:34:57 -1000

    all patient_profile sections use the same routine to show details

--- lib/widgets/patient_profile.dart
+++ lib/widgets/patient_profile.dart
@@ -260,137 +260,146 @@ class BundleEntry extends StatelessWidget {
 //   }
 
   String _entryText(r4.BundleEntry entry, String title) {
-    switch (title) {
-      case 'Conditions':
-        // var _rawEntryResource = entry.resource;
-        // var _entryResource = entry.resource as r4.Condition;
-        // List<String> _codingEntries = ['Codes'];
-        // if (_entryResource.code!.coding != null) {
-        //   for (r4.Coding _codingEntry in _entryResource.code!.coding!) {
-        //     Map<String, dynamic> _jsonCodingEntry = _codingEntry.toJson();
-        //     _jsonCodingEntry.forEach((key, value) {
-        //       _codingEntries.add(key + ':: ' + value);
-        //     });
-        //     if (_codingEntry.code != null)
-        //       _codingEntries.add(' code: ' + _codingEntry.code.toString());
-        //     if (_codingEntry.system != null)
-        //       _codingEntries.add(' system: ' + _codingEntry.system.toString());
-        //     if (_codingEntry.display != null)
-        //       _codingEntries
-        //           .add(' display: ' + _codingEntry.display.toString());
-        //     if (_codingEntry.version != null)
-        //       _codingEntries
-        //           .add(' version: ' + _codingEntry.version.toString());
-        //     if (_codingEntry.userSelected != null)
-        //       _codingEntries.add(
-        //           ' userSelected: ' + _codingEntry.userSelected.toString());
-        //   }
-        // }
-        // if (_entryResource.code!.text != null)
-        //   _codingEntries.add(' text: ' + _entryResource.code!.text!);
-        // // Same for severity.coding ...
-        // List<String> _severityEntries = ['Severities'];
-        // if (_entryResource.severity!.coding != null) {
-        //   for (r4.Coding _entry in _entryResource.severity!.coding!) {
-        //     if (_entry.system != null)
-        //       _severityEntries.add(' system: ' + _entry.system.toString());
-        //     if (_entry.code != null)
-        //       _severityEntries.add(' code: ' + _entry.code.toString());
-        //     if (_entry.display != null)
-        //       _severityEntries.add(' display: ' + _entry.display.toString());
-        //   }
-        // }
-        // if (_entryResource.severity!.text != null)
-        //   _severityEntries.add(' text: ' + _entryResource.severity!.text!);
-        // // Category has two levels of arrays.
-        // List<String> _categoryEntries = ['Categories'];
-        // if (_entryResource.category != null) {
-        //   for (r4.CodeableConcept _category in _entryResource.category!) {
-        //     if (_category.coding != null) {
-        //       for (r4.Coding _entry in _category.coding!) {
-        //         if (_entry.system != null)
-        //           _categoryEntries.add(' system: ' + _entry.system.toString());
-        //         if (_entry.code != null)
-        //           _categoryEntries.add(' code: ' + _entry.code.toString());
-        //         if (_entry.display != null)
-        //           _categoryEntries
-        //               .add(' display: ' + _entry.display.toString());
-        //       }
-        //     }
-        //   }
-        // }
-        // if (_entryResource.category![0].text != null)
-        //   _categoryEntries.add(' text: ' + _entryResource.category![0].text!);
+    // switch (title) {
+    //   case 'Conditions':
+    // var _rawEntryResource = entry.resource;
+    // var _entryResource = entry.resource as r4.Condition;
+    // List<String> _codingEntries = ['Codes'];
+    // if (_entryResource.code!.coding != null) {
+    //   for (r4.Coding _codingEntry in _entryResource.code!.coding!) {
+    //     Map<String, dynamic> _jsonCodingEntry = _codingEntry.toJson();
+    //     _jsonCodingEntry.forEach((key, value) {
+    //       _codingEntries.add(key + ':: ' + value);
+    //     });
+    //     if (_codingEntry.code != null)
+    //       _codingEntries.add(' code: ' + _codingEntry.code.toString());
+    //     if (_codingEntry.system != null)
+    //       _codingEntries.add(' system: ' + _codingEntry.system.toString());
+    //     if (_codingEntry.display != null)
+    //       _codingEntries
+    //           .add(' display: ' + _codingEntry.display.toString());
+    //     if (_codingEntry.version != null)
+    //       _codingEntries
+    //           .add(' version: ' + _codingEntry.version.toString());
+    //     if (_codingEntry.userSelected != null)
+    //       _codingEntries.add(
+    //           ' userSelected: ' + _codingEntry.userSelected.toString());
+    //   }
+    // }
+    // if (_entryResource.code!.text != null)
+    //   _codingEntries.add(' text: ' + _entryResource.code!.text!);
+    // // Same for severity.coding ...
+    // List<String> _severityEntries = ['Severities'];
+    // if (_entryResource.severity!.coding != null) {
+    //   for (r4.Coding _entry in _entryResource.severity!.coding!) {
+    //     if (_entry.system != null)
+    //       _severityEntries.add(' system: ' + _entry.system.toString());
+    //     if (_entry.code != null)
+    //       _severityEntries.add(' code: ' + _entry.code.toString());
+    //     if (_entry.display != null)
+    //       _severityEntries.add(' display: ' + _entry.display.toString());
+    //   }
+    // }
+    // if (_entryResource.severity!.text != null)
+    //   _severityEntries.add(' text: ' + _entryResource.severity!.text!);
+    // // Category has two levels of arrays.
+    // List<String> _categoryEntries = ['Categories'];
+    // if (_entryResource.category != null) {
+    //   for (r4.CodeableConcept _category in _entryResource.category!) {
+    //     if (_category.coding != null) {
+    //       for (r4.Coding _entry in _category.coding!) {
+    //         if (_entry.system != null)
+    //           _categoryEntries.add(' system: ' + _entry.system.toString());
+    //         if (_entry.code != null)
+    //           _categoryEntries.add(' code: ' + _entry.code.toString());
+    //         if (_entry.display != null)
+    //           _categoryEntries
+    //               .add(' display: ' + _entry.display.toString());
+    //       }
+    //     }
+    //   }
+    // }
+    // if (_entryResource.category![0].text != null)
+    //   _categoryEntries.add(' text: ' + _entryResource.category![0].text!);
 
-        // List<String> _entries = [
-        //   if (_entryResource.clinicalStatus != null)
-        //     ' status: ' + _entryResource.clinicalStatus.toString(),
-        //   if (_entryResource.onsetAge!.value != null)
-        //     ' onset age ' + _entryResource.onsetAge!.value.toString(),
-        //   if (_rawEntryResource?.text?.div == null)
-        //     ' narrative: ' +
-        //         html_parser.parseFragment(_rawEntryResource!.text!.div).text!,
-        // ];
-        // List<String> _allEntries = List.from(_entries)
-        //   ..addAll(_severityEntries)
-        //   ..addAll(_codingEntries)
-        //   ..addAll(_categoryEntries);
-        // return _allEntries.join('\n');
-        String _yamlForm = entry.resource!.toYaml(); // YAML version
-        // YamlMap map = loadYaml(_yamlForm);
-        // map.removeWhere((key, value) =>
-        //     key == 'resourceType' || key == 'id' || key == 'meta');
-        Map<String, dynamic> _jsonEntryResource = entry.resource!.toJson();
-        Map<String, dynamic> _filteredJsonEntryResource =
-            Map.from(_jsonEntryResource)
-              ..removeWhere((key, value) =>
-                  key == 'resourceType' ||
-                  key == 'id' ||
-                  key == 'meta'); // json version with filter
-        // return prettyJson(_filteredJsonEntryResource, indent: 2)
-        //     .replaceAll('"', '');
+    // List<String> _entries = [
+    //   if (_entryResource.clinicalStatus != null)
+    //     ' status: ' + _entryResource.clinicalStatus.toString(),
+    //   if (_entryResource.onsetAge!.value != null)
+    //     ' onset age ' + _entryResource.onsetAge!.value.toString(),
+    //   if (_rawEntryResource?.text?.div == null)
+    //     ' narrative: ' +
+    //         html_parser.parseFragment(_rawEntryResource!.text!.div).text!,
+    // ];
+    // List<String> _allEntries = List.from(_entries)
+    //   ..addAll(_severityEntries)
+    //   ..addAll(_codingEntries)
+    //   ..addAll(_categoryEntries);
+    // return _allEntries.join('\n');
+    // String _yamlForm = entry.resource!.toYaml(); // YAML version
+    // YamlMap map = loadYaml(_yamlForm);
+    // map.removeWhere((key, value) =>
+    //     key == 'resourceType' || key == 'id' || key == 'meta');
+    Map<String, dynamic> _jsonEntryResource = entry.resource!.toJson();
+    Map<String, dynamic> _filteredJsonEntryResource =
+        Map.from(_jsonEntryResource)
+          ..removeWhere((key, value) =>
+              key == 'resourceType' ||
+              key == 'id' ||
+              key == 'meta'); // json version with filter
+    // return prettyJson(_filteredJsonEntryResource, indent: 2)
+    //     .replaceAll('"', '');
 //        return _yamlForm;
-        return json2yaml(_filteredJsonEntryResource);
-      case 'Medications':
-        return '${(entry.resource as r4.MedicationStatement).medicationCodeableConcept?.coding?[0].display ?? (entry.resource as r4.MedicationStatement).medicationReference?.display ?? 'Unable to get name'}'
-            .trim();
-      case 'Allergies':
-        return '${(entry.resource as r4.AllergyIntolerance).code?.coding?[0].display ?? '??'}'
-                .trim() +
-            ': ' +
-            '${(entry.resource as r4.AllergyIntolerance).reaction?[0].manifestation[0].coding?[0].display ?? '??'}'
-                .trim();
-      case 'Immunizations':
-        return '${(entry.resource as r4.Immunization).vaccineCode.coding?[0].display ?? '??'}'
-                .trim() +
-            ' (code: ' +
-            '${(entry.resource as r4.Immunization).vaccineCode.coding?[0].code ?? '??'}'
-                .trim() +
-            ')';
-      case 'Imaging Studies':
-        return '${(entry.resource as r4.ImagingStudy).resourceTypeString() ?? '??'}'
-            .trim();
-      case 'Family History':
-        return '${(entry.resource as r4.FamilyMemberHistory).resourceTypeString() ?? '??'}'
-            .trim();
-      case 'Observations':
-        return '${(entry.resource as r4.Observation).code.text ?? '??'}'
-                .trim() +
-            ': ' +
-            '${(entry.resource as r4.Observation).valueQuantity?.value ?? '??'}'
-                .trim() +
-            ' ' +
-            '${(entry.resource as r4.Observation).valueQuantity?.unit ?? '??'}'
-                .trim();
-      case 'Diagnostic Reports':
-        return '${(entry.resource as r4.DiagnosticReport).resourceTypeString() ?? '??'}'
-            .trim();
-      case 'Procedures':
-        return '${(entry.resource as r4.Procedure).resourceTypeString() ?? '??'}'
-            .trim();
-      default:
-        return '';
-    }
+    return json2yaml(_filteredJsonEntryResource);
+    // case 'Medications':
+    //   Map<String, dynamic> _jsonEntryResource = entry.resource!.toJson();
+    //   Map<String, dynamic> _filteredJsonEntryResource =
+    //       Map.from(_jsonEntryResource)
+    //         ..removeWhere((key, value) =>
+    //             key == 'resourceType' ||
+    //             key == 'id' ||
+    //             key == 'meta'); // json version with filter
+    //   return json2yaml(_filteredJsonEntryResource);
+
+    // // return '${(entry.resource as r4.MedicationStatement).medicationCodeableConcept?.coding?[0].display ?? (entry.resource as r4.MedicationStatement).medicationReference?.display ?? 'Unable to get name'}'
+    // //     .trim();
+    // case 'Allergies':
+    //   return '${(entry.resource as r4.AllergyIntolerance).code?.coding?[0].display ?? '??'}'
+    //           .trim() +
+    //       ': ' +
+    //       '${(entry.resource as r4.AllergyIntolerance).reaction?[0].manifestation[0].coding?[0].display ?? '??'}'
+    //           .trim();
+    // case 'Immunizations':
+    //   return '${(entry.resource as r4.Immunization).vaccineCode.coding?[0].display ?? '??'}'
+    //           .trim() +
+    //       ' (code: ' +
+    //       '${(entry.resource as r4.Immunization).vaccineCode.coding?[0].code ?? '??'}'
+    //           .trim() +
+    //       ')';
+    // case 'Imaging Studies':
+    //   return '${(entry.resource as r4.ImagingStudy).resourceTypeString() ?? '??'}'
+    //       .trim();
+    // case 'Family History':
+    //   return '${(entry.resource as r4.FamilyMemberHistory).resourceTypeString() ?? '??'}'
+    //       .trim();
+    // case 'Observations':
+    //   return '${(entry.resource as r4.Observation).code.text ?? '??'}'
+    //           .trim() +
+    //       ': ' +
+    //       '${(entry.resource as r4.Observation).valueQuantity?.value ?? '??'}'
+    //           .trim() +
+    //       ' ' +
+    //       '${(entry.resource as r4.Observation).valueQuantity?.unit ?? '??'}'
+    //           .trim();
+    // case 'Diagnostic Reports':
+    //   return '${(entry.resource as r4.DiagnosticReport).resourceTypeString() ?? '??'}'
+    //       .trim();
+    // case 'Procedures':
+    //   return '${(entry.resource as r4.Procedure).resourceTypeString() ?? '??'}'
+    //       .trim();
+    //   default:
+    //     return '';
+    // }
   }
 
   String _titleText(r4.BundleEntry entry, String title) {
