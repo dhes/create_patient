34c3bbb1464ff2f862869d3ceab52af0fea3a622
Dan Heslinga <dan@heslinga.us>
2021-08-03 14:00:25 -1000

    working on generating patient list from button onPress

---
+++ lib/controllers/list_from_bundle.dart
@@ -0,0 +1,75 @@
+// filter for just patient resources
+// generate patient list
+// return patient list
+import 'package:fhir/r4.dart';
+import 'package:age_calculator/age_calculator.dart';
+import 'package:flutter/foundation.dart' show describeEnum;
+
+List<String> listFromBundle(Bundle? bundleSnapshot) {
+  List<BundleEntry> _entries = bundleSnapshot!.entry as List<BundleEntry>;
+  //var _patients = List<Patient>.from(_entries);
+  List<String> _patientSummary = <String>[];
+  for (BundleEntry _entry in _entries) {
+    String _details;
+    Patient _patient = _entry.resource as Patient;
+    if (_patient.birthDate != null) {
+      var birthday = FhirDateTime(_patient.birthDate.toString());
+      int age = AgeCalculator.age(birthday.value!).years;
+      var dob = FhirDateTime.fromDateTime(
+              DateTime(
+                birthday.value!.year,
+                birthday.value!.month,
+                birthday.value!.day,
+              ),
+              birthday.precision)
+          .toString();
+      var agePrefix = '';
+      if (birthday.precision == DateTimePrecision.YYYYMM ||
+          birthday.precision == DateTimePrecision.YYYY) {
+        agePrefix = '~';
+      }
+      _details = agePrefix +
+          age.toString() +
+          'yo ' +
+          _shortGender(_patient.gender) +
+          ' DOB: ' +
+          dob +
+          ' id: ' +
+          _patient.id.toString();
+    } else {
+      _details = '?? yo ' + _shortGender(_patient.gender) + ' DOB: ????-??-??';
+    }
+
+    // if the 'given' attribute of the first entry in the list of HumanNames
+    // ... is not present then assign it a value of list entry '?' to
+    // ... indicate that it is not known.
+    var _givenName = _patient.name!.first.given ?? ['?'];
+    var _familyName = _patient.name!.first.family ?? '?';
+
+    // nameController.text = _givenName[0] + ' ' + _familyName.toString();
+    var _name = _givenName[0] + ' ' + _familyName.toString();
+
+    _patientSummary.add('$_name $_details');
+  }
+  return _patientSummary;
+}
+
+String _shortGender(PatientGender? longGender) {
+  switch (longGender) {
+    case PatientGender.female:
+      return describeEnum(Gender.F.toString());
+    //break;
+    case PatientGender.male:
+      return describeEnum(Gender.M.toString());
+    //break;
+    case PatientGender.other:
+      return describeEnum(Gender.O.toString());
+    //break;
+    case PatientGender.unknown:
+      return describeEnum(Gender.U.toString());
+    default:
+      return describeEnum(Gender.U.toString());
+  }
+}
+
+enum Gender { F, M, O, U }
--- lib/screens/search_patients.dart
+++ lib/screens/search_patients.dart
@@ -1,5 +1,3 @@
-import 'package:create_patient/screens/display_patient.dart';
-import 'package:create_patient/widgets/patient_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:fhir/r4.dart';
 import '../widgets/small_action_button.dart';
@@ -9,6 +7,8 @@ import '../controllers/main_controller.dart';
 import '../widgets/server_picker.dart';
 import '../widgets/patient_picker.dart';
 import '../controllers/fetch_bundle.dart';
+import '../controllers/list_from_bundle.dart';
+import '../screens/display_patient.dart';
 
 class SearchPatients extends StatelessWidget {
   final PatientGenderController controller = Get.put(PatientGenderController());
@@ -45,6 +45,9 @@ class SearchPatients extends StatelessWidget {
                               lastName: _lastName.text,
                               firstName: _lastName.text,
                             );
+                            List<String> _patientList =
+                                listFromBundle(futureBundle);
+                            debugPrint(_patientList.toString());
                             // debugPrint(futureBundle.toString());
                             // futureBundle
                             //     .then((value) => (value as Bundle).entry);
